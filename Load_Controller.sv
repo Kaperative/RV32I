3b28f14620c5f812160bae1fc0fc5abe3913ff74
Kaperative <ilyahmylko@gmail.com>
2026-05-06 16:27:41 +0300

    Update Load_Controller

--- Load_Controller.sv
+++ Load_Controller.sv
@@ -12,15 +12,14 @@ module Load_Controller (
     logic [15:0] selected_half;
     assign selected_byte = (Address[1:0] == 2'b00) ? ReadDataRaw[7:0]   :
                            (Address[1:0] == 2'b01) ? ReadDataRaw[15:8]  :
-                           (Address[1:0] == 2'b10) ? ReadDataRaw[23:16] :
-                                                     ReadDataRaw[31:24];
+                           (Address[1:0] == 2'b10) ? ReadDataRaw[23:16] : ReadDataRaw[31:24];
 
     assign selected_half = Address[1] ? ReadDataRaw[31:16] : ReadDataRaw[15:0];
-    assign ReadData = (funct3 == FUNCT3_L_LB)  ? { {24{selected_byte[7]}}, selected_byte } : 
-                      (funct3 == FUNCT3_L_LBU) ? { 24'b0, selected_byte }                  : 
-                      (funct3 == FUNCT3_L_LH)  ? { {16{selected_half[15]}}, selected_half }:
-                      (funct3 == FUNCT3_L_LHU) ? { 16'b0, selected_half }                  : 
-                      (funct3 == FUNCT3_L_LW)  ? ReadDataRaw : 32'b0;
-                                                                                       
 
+    assign ReadData = (funct3 == FUNCT3_L_LB)  ? { {24{selected_byte[7]}}, selected_byte }  : 
+                      (funct3 == FUNCT3_L_LBU) ? {  24'b0, selected_byte }                  : 
+                      (funct3 == FUNCT3_L_LH)  ? { {16{selected_half[15]}}, selected_half } :
+                      (funct3 == FUNCT3_L_LHU) ? {  16'b0, selected_half }                  : 
+                      (funct3 == FUNCT3_L_LW)  ?    ReadDataRaw                             : 32'b0;
+                                                                                       
 endmodule
