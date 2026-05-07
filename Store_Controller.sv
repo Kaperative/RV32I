611a5463bc984a0637455391afecca76c887d268
Kaperative <ilyahmylko@gmail.com>
2026-05-08 02:16:32 +0300

    UPDATE. Store_Controller optimized

--- Store_Controller.sv
+++ Store_Controller.sv
@@ -11,17 +11,20 @@ module Store_Controller (
 
     logic [3:0]  sb_mask;    
     logic [3:0]  sh_mask;
+    logic        isSW;
+    logic        isSH;
 
     assign sb_mask = (4'b0001 << Address[1:0]);    
     assign sh_mask = Address[1] ? 4'b1100 : 4'b0011;
-    
-    assign WriteMask = (!MemWrite)             ? 4'b0000 :
-                       (funct3 == FUNCT3_S_SB) ? sb_mask :
-                       (funct3 == FUNCT3_S_SH) ? sh_mask :
-                       (funct3 == FUNCT3_S_SW) ? 4'b1111 : 4'b0000;
 
-    assign WriteData = (funct3 == FUNCT3_S_SB) ? {4{WriteDataRaw[7:0]} } :
-                       (funct3 == FUNCT3_S_SH) ? {2{WriteDataRaw[15:0]}} :
-                       (funct3 == FUNCT3_S_SW) ?    WriteDataRaw         : 32'b0;
+    assign isSW    = funct3[1];
+    assign isSH    = funct3[0];
+    
+    assign WriteMask =  (isSW) ? 4'b1111 :
+                        (isSH) ? sh_mask : sb_mask; 
+
+    assign WriteData =  (isSW) ?       WriteDataRaw         :
+                        (isSH) ?    {2{WriteDataRaw[15:0]}} : 
+                                    {4{WriteDataRaw[7:0]} } ; // SB - default
 
 endmodule
