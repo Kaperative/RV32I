fcd0c6bb5aac79d73888fafd056cf17399f0d6ba
Kaperative <ilyahmylko@gmail.com>
2026-05-08 01:55:04 +0300

    UPDATE. Load_Controller optimized

--- Load_Controller.sv
+++ Load_Controller.sv
@@ -10,16 +10,28 @@ module Load_Controller (
 
     logic [7:0]  selected_byte;
     logic [15:0] selected_half;
+
+    logic        isUnsigned;
+    logic        isLW;
+    logic        isLHx;
+
+
+
+
     assign selected_byte = (Address[1:0] == 2'b00) ? ReadDataRaw[7:0]   :
                            (Address[1:0] == 2'b01) ? ReadDataRaw[15:8]  :
                            (Address[1:0] == 2'b10) ? ReadDataRaw[23:16] : ReadDataRaw[31:24];
 
-    assign selected_half = Address[1] ? ReadDataRaw[31:16] : ReadDataRaw[15:0];
+    assign selected_half =  Address[1] ? ReadDataRaw[31:16] : ReadDataRaw[15:0];
 
-    assign ReadData = (funct3 == FUNCT3_L_LB)  ? { {24{selected_byte[7]}}, selected_byte }  : 
-                      (funct3 == FUNCT3_L_LBU) ? {  24'b0, selected_byte }                  : 
-                      (funct3 == FUNCT3_L_LH)  ? { {16{selected_half[15]}}, selected_half } :
-                      (funct3 == FUNCT3_L_LHU) ? {  16'b0, selected_half }                  : 
-                      (funct3 == FUNCT3_L_LW)  ?    ReadDataRaw                             : 32'b0;
+    assign isUnsigned = funct3[2];
+    assign isLW       = funct3[1];
+    assign isLHx      = funct3[0];
+
+    assign ReadData = (isLW)                   ?    ReadDataRaw                             :
+                      (isLHx & !isUnsigned)    ? { {16{selected_half[15]}}, selected_half } :
+                      (isLHx &  isUnsigned)    ? {  16'b0, selected_half }                  :                               
+                      (!isUnsigned)            ? { {24{selected_byte[7]}}, selected_byte }  : 
+                      (isUnsigned)             ? {  24'b0, selected_byte }                  :   32'b0;
                                                                                        
 endmodule
